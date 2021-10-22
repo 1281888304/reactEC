abbe6c0ad06a5358f58ac63f3d6b489c115b4970
QINGHANG ZHANG <Qzhang9@mail.greenriver.edu>
2021-10-22 15:14:41 -0700

    done extra credit

---
+++ qinghang_zhang_react_ec/src/ECbody.jsx
@@ -0,0 +1,29 @@
+import './ECbody.css'
+export default function(){
+    return <div class="grid-container fullScreen">
+    <header class="header">
+        <h1>Qinghang's Website</h1>
+    </header>
+    <div class="leftBar">
+        <div class="list">
+            <li><a href="#">Home Page</a></li>
+            <li><a href="#">Photos</a></li>
+            <li><a href="#">Select Projects</a></li>
+        </div>
+        
+
+    </div>
+    <div class="middleBar">
+        <div class="box"></div>
+    </div>
+    <div class="rightBar">
+
+
+    </div>
+    <div class="footer">
+
+    </div>
+    
+    
+</div>;
+}
---
+++ qinghang_zhang_react_ec/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ECbody from './ECbody';
+import './index.css';
+
+
+
+ReactDOM.render(
+  
+  <ECbody />,
+  document.getElementById('root')
+);
+
+
